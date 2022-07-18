dc5ff2e8788742f99b9e7bae5a73318123002e03
liuwei <1097408921@qq.com>
2022-07-18 21:08:17 +0800

    初次提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer:{
+    host:'localhost',
+    port:'8080',
+    open:true
+  },
   transpileDependencies: true
 })
